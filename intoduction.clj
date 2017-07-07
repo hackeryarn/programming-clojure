aecbf59ca0279af55d01d01bbe18ac3e03610fc3
achernyak <artemchernyak@gmail.com>
2017-07-07 07:32:44 -0500

    function

--- intoduction.clj
+++ intoduction.clj
@@ -1,4 +1,5 @@
-(ns introduction)
+(ns introduction
+  (:require [clojure.string :as str]))
 
 (defn blank? [str]
   (every? #(Character/isWhitespace %) str))
@@ -17,3 +18,19 @@
   [username]
   (swap! visitors conj username)
   (str "Hello, ", username))
+
+(defrecord Book [title author])
+
+(->Book "title" "author")
+
+(defn greeting
+  "Returns a greeting of the form 'Hello, username.'
+  Default username is 'world'."
+  ([] (greeting "world"))
+  ([username] (str "Hello, " username)))
+
+(defn date [person-1 person-2 & chaperones]
+  (println person-1 "and" person-2
+           "went out with" (count chaperones) "chaperones."))
+
+(filter #(> (count %) 2) (str/split "A fine day it is" #"\W+"))
