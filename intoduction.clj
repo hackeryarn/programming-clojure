d04c0b7bde9d4b53ca2b803824f1aae241aa59de
achernyak <artemchernyak@gmail.com>
2017-07-10 06:52:01 -0500

    exploring

--- intoduction.clj
+++ intoduction.clj
@@ -34,3 +34,40 @@
            "went out with" (count chaperones) "chaperones."))
 
 (filter #(> (count %) 2) (str/split "A fine day it is" #"\W+"))
+
+(defn is-samll? [number]
+  (if (< number 100)
+    "yes"
+    (do
+      (println "Saw a big number" number)
+      "no")))
+
+(is-samll? 5000)
+
+(loop [result [] x 5]
+  (if (zero? x)
+    result
+    (recur (conj result x) (dec x))))
+
+(defn countdown [result x]
+  (if (zero? x)
+    result
+    (recur (conj result x) (dec x))))
+
+(countdown [] 5)
+
+(defn indexed [coll] (map-indexed vector coll))
+
+(indexed "abcde")
+
+(defn index-filter [pred coll]
+  (when pred
+    (for [[idx elt] (indexed coll) :when (pred elt)] idx)))
+
+(defn index-of-any [pred coll]
+  (first (index-filter pred coll)))
+
+(index-of-any #{\z \a} "zzabyycdxx")
+
+
+(index-of-any #{\b \y} "zzabyycdxx")
