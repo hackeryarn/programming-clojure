622ab62044064e0a4757bcabf6439d8857870133
achernyak <artemchernyak@gmail.com>
2017-07-11 07:54:41 -0500

    more seqs

--- seqs.clj
+++ seqs.clj
@@ -28,3 +28,44 @@
 (apply str (interpose "," ["apples" "bananas" "grapes"]))
 
 (join \, ["apples" "bananas" "grapes"])
+
+(take 10 (filter even? whole-numbers))
+
+(def vowel? #{\a\e\i\o\u})
+(def consonant? (complement vowel?))
+
+(take-while consonant? "the-quick-brown-fox")
+
+(drop-while consonant? "the-quick-brown-fox")
+
+(split-at 5 (range 10))
+
+(split-with #(<= % 10) (range 0 20 2))
+
+(every? odd? [1 3 5])
+
+(every? odd? [1 3 5 8])
+
+(some even? [1 3 5])
+
+(some even? [1 2 5])
+
+(map #(format "<p>%s</p>" %) ["the" "quick" "brown" "fox"])
+
+(map #(format "<%s>%s</%s>" %1 %2 %1)
+     ["h1" "h2" "h3" "h1"] ["the" "quick" "brown" "fox"])
+
+(reduce + (range 1 11))
+
+(reduce * (range 1 11))
+
+(sort [42 1 7 11])
+
+(sort-by #(.toString %) [42 1 7 11])
+
+(for [word ["the" "quick" "brown" "fox"]]
+  (format "<p>%s</p>" word))
+
+(for [file "ABCDEFGH"
+      rank (range 1 9)]
+  (format "%c%d" file rank))
