288093a9374fee8ab22069e65c20fa5b6d4fc9c1
achernyak <artemchernyak@gmail.com>
2017-07-19 06:39:43 -0500

    added project file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject programming-clojure "1.9.0"
+  :description "My code from Programming Clojure"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/test.check "0.9.0"]])
