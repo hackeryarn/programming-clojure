3f2dc2235888d733c0123373ea88a241c23f40e2
achernyak <artemchernyak@gmail.com>
2017-07-20 08:09:09 -0500

    value specs

--- spec.clj
+++ spec.clj
@@ -14,3 +14,31 @@
 
 (defn scale-ingredient [ingredient factor]
   (update ingredient :quantity * factor))
+
+(s/def ::marble-color #{:red :green :blue})
+(s/valid? ::marble-color :red)
+(s/valid? ::marble-color :pink)
+
+(s/def ::bowling-roll (s/int-in 0 11))
+(s/valid? ::bowling-roll 10)
+
+(s/def ::company-name-2 (s/nilable string?))
+
+(s/valid? ::company-name-2 nil)
+
+(s/def ::odd-int (s/and int? odd?))
+(s/valid? ::odd-int 5)
+
+(s/valid? ::odd-int 10)
+
+(s/valid? ::odd-int 5.2)
+
+(s/def ::odd-or-42 (s/or :odd ::odd-int :42 #{42}))
+
+(s/conform ::odd-or-42 42)
+
+(s/conform ::odd-or-42 19)
+
+(s/explain ::odd-or-42 0)
+
+
