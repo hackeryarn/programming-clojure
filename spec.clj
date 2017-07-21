6bd44b26819a260bf37df5ee6c91ed6af811105b
achernyak <artemchernyak@gmail.com>
2017-07-21 07:02:40 -0500

    validating maps

--- spec.clj
+++ spec.clj
@@ -9,8 +9,8 @@
 (s/def ::unit keyword?)
 
 (s/fdef scale-ingredient
-  :args (s/cat :ingredient ::ingredient :factor number?)
-  :ret ::ingredient)
+        :args (s/cat :ingredient ::ingredient :factor number?)
+        :ret ::ingredient)
 
 (defn scale-ingredient [ingredient factor]
   (update ingredient :quantity * factor))
@@ -41,4 +41,40 @@
 
 (s/explain ::odd-or-42 0)
 
+(s/def ::names (s/coll-of string?))
+
+(s/valid? ::names ["Alex" "Stu"])
+
+(s/valid? ::names #{"Alex" "Stu"})
+
+(s/valid? ::names '("Alex" "Stu"))
+
+(s/def ::my-set (s/coll-of int? :kind set? :min-count 2))
+(s/valid? ::my-set #{10 20})
+
+(s/def ::scores (s/map-of string? int?))
+(s/valid? ::scores {"Stu" 100, "Alex" 200})
+
+(s/def ::point (s/tuple rational? rational?))
+
+(s/conform ::point [1.3 2.7])
+
+(s/def ::music-id uuid?)
+(s/def ::music-artist string?)
+(s/def ::music-title string?)
+(s/def ::music-date inst?)
+
+(s/def ::music-release
+  (s/keys :req [::music-id]
+          :opt [::music-artist
+                ::musict-title
+                ::music-date]))
+
+
+;; unqualified are used for keys of any namespace
+(s/def ::music-release-unqualified
+  (s/keys :req-un [::music-id]
+          :opt-un [::music-artist
+                   ::music-title
+                   ::music-date]))
 
