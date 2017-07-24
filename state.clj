94d05235ffcc69b9506b75d3d6180785cfb8ba11
achernyak <artemchernyak@gmail.com>
2017-07-24 07:00:45 -0500

    atoms

--- state.clj
+++ state.clj
@@ -14,4 +14,18 @@
  (ref-set current-track "Credo")
  (ref-set current-track "Byrd"))
 
+(def current-track (atom "Venus, the Bringer of Peace"))
+
+@current-track
+
+(reset! current-track "Credo")
+
+(def current-track (atom {:title "Credo" :composer "Byrd"}))
+
+(reset! current-track {:title "Spem in Alium" :composer "Tallis"})
+
+(swap! current-track assoc :title "Sancte Deus")
+
+(def counter (agent 0))
+
 
