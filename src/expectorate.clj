40eded30a0306e4fb9157fc66d7d99b4233730a0
achernyak <artemchernyak@gmail.com>
2017-07-26 06:35:52 -0500

    imple gulp and expectorate

---
+++ src/expectorate.clj
@@ -0,0 +1,10 @@
+(ns expectorate
+  (:import (java.io FileOutputStream OutputStreamWriter BufferedWriter)))
+
+(defn expectorate [dst content]
+  (with-open [writer (-> dst
+                         FileOutputStream.
+                         OutputStreamWriter.
+                         BufferedWriter.)]
+    (.write writer (str content))))
+
