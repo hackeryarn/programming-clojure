3849e0705899281a5f152c846f1ac5ef18c357bd
achernyak <artemchernyak@gmail.com>
2017-08-02 07:45:56 -0500

    datatypes

--- src/protocols.clj
+++ src/protocols.clj
@@ -1,6 +1,11 @@
 (ns protocols
   (:import [java.io BufferedReader File FileInputStream InputStream InputStreamReader]
-           [java.net Socket URL]))
+           [java.net Socket URL]
+           [java.security KeyStore KeyStore$SecretKeyEntry
+            KeyStore$PasswordProtection]
+           [javax.crypto KeyGenerator Cipher CipherOutputStream
+            CipherInputStream])
+  (:require [clojure.java.io :as io]))
 
 ;; (defn make-reader [src]
 ;;   (-> (condp = (type src)
@@ -88,3 +93,48 @@
        (-> dst .getPath FileInputStream.)
        (throw (IllegalArgumentException.
                "Can't write to non-file URL"))))))
+
+(defn vault-key [vault]
+  (let [password (.toCharArray (.password vault))]
+    (with-open [fis (FileInputStream. (.keystore vault))]
+      (-> (doto (KeyStore/getInstance "JCEKS")
+            (.load fis password))
+          (.getKey "vault-key" password)))))
+
+(defprotocol Vault
+  (init-vault [vault])
+  (vault-output-stream [vault])
+  (vault-input-stream [vault]))
+
+(deftype CryptoVault [filename keystore password]
+  Vault
+  (init-vault [vault]
+    (let [password (.toCharArray (.password vault))
+          key (.generateKey (KeyGenerator/getInstance "AES"))
+          keystore (doto (KeyStore/getInstance "JCEKS")
+                     (.load nil password)
+                     (.setEntry "vault-key"
+                                (KeyStore$SecretKeyEntry. key)
+                                (KeyStore$PasswordProtection. password)))]
+      (with-open [fos (FileOutputStream. (.keystore vault))]
+        (.store keystore fos password))))
+  (vault-output-stream [vault]
+    (let [cipher (doto (Cipher/getInstance "AES")
+                   (.init Cipher/ENCRYPT_MODE (vault-key vault)))]
+      (CipherOutputStream. (io/output-stream (.filename vault)) cipher)))
+  (vault-input-stream [vault]
+    (let [cipher (doto (Cipher/getInstance "AES")
+                   (.init Cipher/DECRYPT_MODE (vault-key vault)))]
+      (CipherInputStream. (io/input-stream (.filename vault)) cipher)))
+
+  IOFactory
+  (make-reader [vault]
+    (make-reader (vault-input-stream vault)))
+  (make-writer [vault]
+    (make-writer (vault-output-stream vault))))
+
+(extend CryptoVault
+  clojure.java.io/IOFactory
+  (assoc clojure.java.io/default-streams-impl
+         :make-input-stream (fn [x opts] (vault-input-stream x))
+         :make-output-stream (fn [x opts] (vault-output-stream x))))
